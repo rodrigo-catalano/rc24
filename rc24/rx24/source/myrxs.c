0dc6b8d06e742c1cdba05be2668ebfb7bb0f45f0
Gbloice <Gbloice@cf8e452e-b714-11de-b7b1-c13d6a5ff25b>
2009-10-19 22:07:14 +0000

    Code tidy, comments and TODOs.

--- rc24/rx24/source/myrxs.c
+++ rc24/rx24/source/myrxs.c
@@ -1,23 +1,23 @@
 /*
-Copyright 2008 - 2009 © Alan Hopper
+ Copyright 2008 - 2009 © Alan Hopper
 
-	This file is part of rc24.
+ This file is part of rc24.
 
-    rc24 is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
+ rc24 is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
 
-    rc24 is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+ rc24 is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
 
-    You should have received a copy of the GNU General Public License
-    along with rc24.  If not, see <http://www.gnu.org/licenses/>.
+ You should have received a copy of the GNU General Public License
+ along with rc24.  If not, see <http://www.gnu.org/licenses/>.
 
 
-*/
+ */
 
 #include <jendefs.h>
 #include <AppHardwareApi.h>
@@ -30,204 +30,301 @@ Copyright 2008 - 2009 © Alan Hopper
 
 #include <printf.h>
 
+/****************************************************************************/
+/***        Local Variables                                               ***/
+/****************************************************************************/
 
 // map inputs from the tx to outputs from the rx
-// outputs can currently be servo pwm,
-// constant pwm suitable for simple motor speed control (2 only on timer outputs )
-// or on/off digital outputs
+// outputs can currently be;
+//	servopwm - for servo outputs, 0 - 4096 from tx -> 1.0 - 2.0 ms pulse width
+//  pwm  - suitable for simple motor speed control (2 only on timer outputs)
+//  digital - digital output, > 3000 from tx -> on
+//  i2c - i2c i/o
+//  nc - not connected
 
-// set up for 6ch standard servo output
-opChannel standard6channel[20]={ {servopwm,E_AHI_DIO10_INT},
-                                 {servopwm,E_AHI_DIO13_INT},
-                                 {servopwm,E_AHI_DIO6_INT},
-                                 {servopwm,E_AHI_DIO7_INT},
-                                 {servopwm,E_AHI_DIO14_INT},
-                                 {servopwm,E_AHI_DIO15_INT},
-                                 {nc,0},
-                                 {nc,0},
-                                 {nc,0},
-                                 {nc,0},
-                                 {nc,0},
-                                 {nc,0},
-                                 {nc,0},
-                                 {nc,0},
-                                 {nc,0},
-                                 {nc,0},
-                                 {nc,0},
-                                 {nc,0},
-                                 {nc,0},
-                                 {nc,0}};
+// Set up for 6ch standard servo output
+opChannel standard6channel[20] = {
+	{ servopwm, E_AHI_DIO10_INT},
+	{ servopwm, E_AHI_DIO13_INT},
+	{ servopwm, E_AHI_DIO6_INT},
+	{ servopwm, E_AHI_DIO7_INT},
+	{ servopwm, E_AHI_DIO14_INT},
+	{ servopwm, E_AHI_DIO15_INT},
+	{ nc, 0},
+	{ nc, 0},
+	{ nc, 0},
+	{ nc, 0},
+	{ nc, 0},
+	{ nc, 0},
+	{ nc, 0},
+	{ nc, 0},
+	{ nc, 0},
+	{ nc, 0},
+	{ nc, 0},
+	{ nc, 0},
+	{ nc, 0},
+	{ nc, 0 }
+};
 
 // set up for 6ch standard servo output with serial debug
-opChannel standard6channelDebug[20]={ {servopwm,E_AHI_DIO9_INT},
-                                 {servopwm,E_AHI_DIO14_INT},
-                                 {servopwm,E_AHI_DIO15_INT},
-                                 {servopwm,E_AHI_DIO13_INT},
-                                 {nc,0},
-                                 {nc,0},
-                                 {nc,0},
-                                 {nc,0},
-                                 {nc,0},
-                                 {nc,0},
-                                 {nc,0},
-                                 {nc,0},
-                                 {nc,0},
-                                 {nc,0},
-                                 {nc,0},
-                                 {nc,0},
-                                 {nc,0},
-                                 {nc,0},
-                                 {nc,0},
-                                 {nc,0}};
+opChannel standard6channelDebug[20] = {
+	{ servopwm, E_AHI_DIO9_INT},
+	{ servopwm, E_AHI_DIO14_INT},
+	{ servopwm, E_AHI_DIO15_INT},
+	{ servopwm, E_AHI_DIO13_INT},
+	{ nc, 0},
+	{ nc, 0},
+	{ nc, 0},
+	{ nc, 0},
+	{ nc, 0},
+	{ nc, 0},
+	{ nc, 0},
+	{ nc, 0},
+	{ nc, 0},
+	{ nc, 0},
+	{ nc, 0},
+	{ nc, 0},
+	{ nc, 0},
+	{ nc, 0},
+	{ nc, 0},
+	{ nc, 0 }
+};
 
 // setup for 2 servos and two brushed motors
-opChannel propsteer[20]={        {servopwm,E_AHI_DIO6_INT},
-                                 {servopwm,E_AHI_DIO7_INT},
-                                 {pwm,E_AHI_DIO10_INT},
-                                 {pwm,E_AHI_DIO13_INT},
-                                 {nc,0},
-                                 {nc,0},
-                                 {nc,0},
-                                 {nc,0},
-                                 {nc,0},
-                                 {nc,0},
-                                 {nc,0},
-                                 {nc,0},
-                                 {nc,0},
-                                 {nc,0},
-                                 {nc,0},
-                                 {nc,0},
-                                 {nc,0},
-                                 {nc,0},
-                                 {nc,0},
-                                 {nc,0}};
+opChannel propsteer[20] = {
+	{ servopwm, E_AHI_DIO6_INT},
+	{ servopwm, E_AHI_DIO7_INT},
+	{ pwm, E_AHI_DIO10_INT},
+	{ pwm, E_AHI_DIO13_INT},
+	{ nc, 0},
+	{ nc, 0},
+	{ nc, 0},
+	{ nc, 0},
+	{ nc, 0},
+	{ nc, 0},
+	{ nc, 0},
+	{ nc, 0},
+	{ nc, 0},
+	{ nc, 0},
+	{ nc, 0},
+	{ nc, 0},
+	{ nc, 0},
+	{ nc, 0},
+	{ nc, 0},
+	{ nc, 0}
+};
+
 // setup for 4 servos and gps
-opChannel gps[20]=      {        {servopwm,E_AHI_DIO10_INT},
-                                 {servopwm,E_AHI_DIO13_INT},
-                                 {servopwm,E_AHI_DIO14_INT},
-                                 {servopwm,E_AHI_DIO15_INT},
-                                 {nc,0},
-                                 {nc,0},
-                                 {nc,0},
-                                 {nc,0},
-                                 {nc,0},
-                                 {nc,0},
-                                 {nc,0},
-                                 {nc,0},
-                                 {nc,0},
-                                 {nc,0},
-                                 {nc,0},
-                                 {nc,0},
-                                 {nc,0},
-                                 {nc,0},
-                                 {nc,0},
-                                 {nc,0}};
+opChannel gps[20] = {
+	{ servopwm, E_AHI_DIO10_INT},
+	{ servopwm, E_AHI_DIO13_INT},
+	{ servopwm, E_AHI_DIO14_INT},
+	{ servopwm, E_AHI_DIO15_INT},
+	{ nc, 0},
+	{ nc, 0},
+	{ nc, 0},
+	{ nc, 0},
+	{ nc, 0},
+	{ nc, 0},
+	{ nc, 0},
+	{ nc, 0},
+	{ nc, 0},
+	{ nc, 0},
+	{ nc, 0},
+	{ nc, 0},
+	{ nc, 0},
+	{ nc, 0},
+	{ nc, 0},
+	{ nc, 0}
+};
 
-opChannel* outputMapping;
+// The output mapping in use
+PRIVATE opChannel* outputMapping;
 
-void initTxMac(uint32* machigh,uint32* maclow)
+/****************************************************************************
+ *
+ * NAME: initTxMac
+ *
+ * DESCRIPTION: Set the Tx MAC in use
+ *
+ * PARAMETERS:      Name            RW  Usage
+ *					machigh			W	The Tx MAC high word
+ *					maclow			W	The Tx MAX low word
+ *
+ * RETURNS:
+ *  None.
+ *
+ * NOTES:
+ *  None.
+ ****************************************************************************/
+void initTxMac(uint32* machigh, uint32* maclow)
 {
-    //enter the mac address of your transmitter here
-    *maclow=0x000a5f1f;
-    *machigh=0x00158d00;
+	// Enter the mac address of your transmitter here
+	// TODO - Do we *have* to hard-code the TX MAC?
+	*maclow = 0x000a5f1f;
+	*machigh = 0x00158d00;
 }
 
-void initInputs()
+/****************************************************************************
+ *
+ * NAME: initInputs
+ *
+ * DESCRIPTION: Initialise receiver inputs
+ *
+ * PARAMETERS:      Name            RW  Usage
+ *  None.
+ *
+ * RETURNS:
+ *  None.
+ *
+ * NOTES:
+ *  None.
+ ****************************************************************************/
+void initInputs(void)
 {
-     //comment out if no gps connected
- //    initNmeaGps(E_AHI_UART_0,E_AHI_UART_RATE_38400);
+	// TODO - allow conditional compilation
+	// comment out if no gps connected
+	//initNmeaGps(E_AHI_UART_0,E_AHI_UART_RATE_38400);
 }
 
+/****************************************************************************
+ *
+ * NAME: initOutputs
+ *
+ * DESCRIPTION: Initialise outputs
+ *
+ * PARAMETERS:      Name            RW  Usage
+ *  None.
+ *
+ * RETURNS:
+ *  None.
+ *
+ * NOTES:
+ *  None.
+ ****************************************************************************/
 void initOutputs()
 {
-    int nservos=0;
+	int nservos = 0;	// Count of number of configured servos
 
-    int i;
-    //set outputs used
-//    outputMapping=standard6channel;
-    outputMapping=standard6channelDebug;
-   // outputMapping=gps;
-  //  outputMapping=propsteer;
+	// Set output mapping used
+	//outputMapping=standard6channel;
+	outputMapping = standard6channelDebug;
+	//outputMapping=gps;
+	//outputMapping=propsteer;
 
-    for(i=0;i<20;i++)
-    {
-        switch(outputMapping[i].type)
-        {
-            case servopwm:
-            {
-                setServoBit(nservos++,outputMapping[i].pin);
-                break;
-            }
-            case pwm:
-            {
-                initMotorPwm(outputMapping[i].pin);
-                 break;
-            }
-            case digital:
-            {
-                vAHI_DioSetDirection(0,outputMapping[i].pin);
-                vAHI_DioSetOutput(0,outputMapping[i].pin);
-                break;
-            }
-            case i2c:
-            {
-                //not implemented
-                break;
-            }
-            case nc:
-            {
+	int i;
+	for (i = 0; i < 20; i++)
+	{
+		// For each of the configured output types
+		switch (outputMapping[i].type)
+		{
+		case servopwm:
+		{
+			setServoBit(nservos++, outputMapping[i].pin);
+			break;
+		}
+		case pwm:
+		{
+			initMotorPwm(outputMapping[i].pin);
+			break;
+		}
+		case digital:
+		{
+			vAHI_DioSetDirection(0, outputMapping[i].pin);
+			vAHI_DioSetOutput(0, outputMapping[i].pin);
+			break;
+		}
+		case i2c:
+		{
+			//not implemented
+			break;
+		}
+		case nc:
+		{
 
-                break;
-            }
-        }
-    }
-    initServoPwm(nservos);
+			break;
+		}
+		}
+	}
+
+	// Initialise the servo pwm driver
+	// TODO - only call if nServos > 0??
+	initServoPwm(nservos);
 }
 
-void updateOutputs( uint16* channelData )
+/****************************************************************************
+ *
+ * NAME: updateOutputs
+ *
+ * DESCRIPTION: Update outputs with new values from Tx
+ *
+ * PARAMETERS:      Name            RW  Usage
+ *					channelData		R	array of new chanel data
+ *
+ * RETURNS:
+ *  None.
+ *
+ * NOTES:
+ *  None.
+ ****************************************************************************/
+void updateOutputs(uint16* channelData)
 {
-    int i;
-    //do any mixing, control loops etc
+	// Do any mixing, control loops etc that are done in Rx
 
+	// Set outputs
 
-    //set outputs
+	int servo = 0;	// Index into servo array
+	int i;
+	for (i = 0; i < 20; i++)
+	{
+		// Ignore channels not received yet
+		if (channelData[i] >= 4096)
+			continue;
 
-    int servo=0;
-    for(i=0;i<20;i++)
-    {
-        if(channelData[i]>=4096) continue;//ignore channels not received yet
-        switch(outputMapping[i].type)
-        {
-            case servopwm:
-            {
-                uint32 u32Demand;
-                u32Demand = channelData[i]*16000/4096+16000;
-                if(u32Demand > 2000*16)u32Demand=2000*16;
-                if(u32Demand < 1000*16)u32Demand=1000*16;
-                setServoDemand(servo++,u32Demand);
-                break;
-            }
-            case pwm:
-            {
-                setMotorPWM(outputMapping[i].pin,channelData[i]*16000/4096 );
-                break;
-            }
-            case digital:
-            {
-                if(channelData[i]>3000)vAHI_DioSetOutput(outputMapping[i].pin,0);
-                else vAHI_DioSetOutput(0,outputMapping[i].pin);
-                break;
-            }
-            case i2c:
-            {
-                //not implemented
-                break;
-            }
-            case nc:
-            {
+		// Iterate over each output
+		switch (outputMapping[i].type)
+		{
+		case servopwm:
+		{
+			// Set a servo pwm output
 
-                break;
-            }
-        }
-    }
+			// Convert the demand to a pulse width in uS
+			uint32 u32Demand = channelData[i] * 16000 / 4096 + 16000;
+
+			// Limit the pulse width to 1-2mS
+			// TODO - allow travel over limits, e.g. 0.9 - 2.1
+			if (u32Demand > 2000 * 16)
+				u32Demand = 2000 * 16;
+			if(u32Demand < 1000 * 16)
+				u32Demand = 1000 * 16;
+
+			// Set the demanded pulse width
+			setServoDemand(servo++, u32Demand);
+			break;
+		}
+		case pwm:
+		{
+			// Set the motor PWM demanded value
+			setMotorPWM(outputMapping[i].pin, channelData[i] * 16000 / 4096);
+			break;
+		}
+		case digital:
+		{
+			if(channelData[i] > 3000)
+				vAHI_DioSetOutput(outputMapping[i].pin, 0);
+			else
+				vAHI_DioSetOutput(0, outputMapping[i].pin);
+			break;
+		}
+		case i2c:
+		{
+			//not implemented
+			break;
+		}
+		case nc:
+		{
+			break;
+		}
+		}
+	}
 }
