088d40fd8e51b31b38a522c09f07c4b3df6063a2
Gbloice <Gbloice@cf8e452e-b714-11de-b7b1-c13d6a5ff25b>
2009-10-19 22:07:14 +0000

    Code tidy, comments and TODOs.

--- trunk/rc24/rx24/source/myrxs.h
+++ trunk/rc24/rx24/source/myrxs.h
@@ -1,41 +1,54 @@
 /*
-Copyright 2008 - 2009 © Alan Hopper
+ Copyright 2008 - 2009 © Alan Hopper
 
-	This file is part of rc24.
+ This file is part of rc24.
 
-    rc24 is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
+ rc24 is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
 
-    rc24 is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+ rc24 is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
 
-    You should have received a copy of the GNU General Public License
-    along with rc24.  If not, see <http://www.gnu.org/licenses/>.
-
-
-*/
+ You should have received a copy of the GNU General Public License
+ along with rc24.  If not, see <http://www.gnu.org/licenses/>.
 
 
- typedef enum
- {
-    servopwm,
-    pwm,
-    digital,
-    i2c,
-    nc,
-}opType;
+ */
 
+/****************************************************************************/
+/***        Exported type definitions				  					  ***/
+/****************************************************************************/
+
+// TODO - Do these need to be public?  Only used in myrxs.c
+// The type of outputs supported
+typedef enum
+{
+	servopwm, pwm, digital, i2c, nc
+} opType;
+
+// Output channel definition
 typedef struct
 {
-    opType type;
-    uint32 pin;
-}opChannel;
+	opType type;
+	uint32 pin;
+} opChannel;
 
-void initTxMac(uint32* machigh,uint32* maclow);
-void initInputs(void );
-void initOutputs(void );
-void updateOutputs( uint16* channelData );
+/****************************************************************************/
+/***        Exported Function Prototypes                                  ***/
+/****************************************************************************/
+
+// Initialise the Tx MAC address
+void initTxMac(uint32* machigh, uint32* maclow);
+
+// Initialise the receiver inputs
+void initInputs(void);
+
+// Initialise the receiver outputs
+void initOutputs(void);
+
+// Update the outputs
+void updateOutputs(uint16* channelData);
